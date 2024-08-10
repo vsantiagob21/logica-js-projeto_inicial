7e256be07cbe4786ba5be8cbd0f7077d407a1478
VictoriaSAlfeu <vsantiagob21@gmail.com>
2024-08-10 18:25:59 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("Bem vindo(a) ao jogo do número secreto")
+let numeroSecreto = 29
+let chute = prompt("Escolha um número entre 1 e 30")
+
+if (chute == numeroSecreto) {
+    console.log("Vocë descobriu o número secreto (29)")
+}
